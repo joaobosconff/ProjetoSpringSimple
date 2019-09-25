3eb38290af8541662c9bc61410096dc246323f12
João Bosco <joao.filho@maisunifacisa.com.br>
2019-09-25 17:40:21 -0300

    first commit

---
+++ src/main/java/br/facisa/lsi/ProjetoSpringSimple/ProjetoSpringSimpleApplication.java
@@ -0,0 +1,13 @@
+package br.facisa.lsi.ProjetoSpringSimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/facisa/lsi/ProjetoSpringSimple/ProjetoSpringSimpleApplicationTests.java
@@ -0,0 +1,16 @@
+package br.facisa.lsi.ProjetoSpringSimple;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetoSpringSimpleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
